d06a6e3d9da09fe757c2875e5d2b873b12bc9e66
Raphael Vigee <contact@raphaelvigee.com>
2017-12-11 17:52:24 +0000

    Fix fields access

--- src/main/java/Framework/Routing/ActionHandlerInterface.java
+++
@@ -1,9 +0,0 @@
-package Framework.Routing;
-
-import Framework.Exception.UnhandledParameterException;
-
-@FunctionalInterface
-public interface ActionHandlerInterface<A, B, C, R>
-{
-    R apply(A var1, B var2, C var3) throws UnhandledParameterException;
-}
--- src/main/java/Framework/Routing/ActionInterface.java
+++ src/main/java/Framework/Routing/ActionInterface.java
@@ -1,7 +1,9 @@
 package Framework.Routing;
 
 import Framework.Container.Container;
+import Framework.Exception.UnhandledParameterException;
 
-public interface ActionInterface extends ActionHandlerInterface<Container, HTTPSession, Route, Response>
+public interface ActionInterface
 {
+    Response apply(Container c, HTTPSession s, Route r) throws UnhandledParameterException;
 }
