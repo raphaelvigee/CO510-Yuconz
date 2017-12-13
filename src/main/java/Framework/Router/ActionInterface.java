0e62775287f1d522e1ace0e5e8269fd5690f25ba
Raphael Vigee <contact@raphaelvigee.com>
2017-12-13 16:23:38 +0000

    Ass FunctionnalInterface annotation

--- src/main/java/Framework/Router/ActionInterface.java
+++ src/main/java/Framework/Router/ActionInterface.java
@@ -4,6 +4,7 @@ import Framework.Container.Container;
 import Framework.Exception.UnhandledParameterException;
 import Framework.Server.HTTPSession;
 
+@FunctionalInterface
 public interface ActionInterface
 {
     Response apply(Container c, HTTPSession s, Route r) throws UnhandledParameterException;
