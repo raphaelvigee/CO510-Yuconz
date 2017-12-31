82881d73158794fd16242205dd2be618f0b52847
Raphael Vigee <contact@raphaelvigee.com>
2017-12-31 14:34:16 +0100

    Test invalid response

--- src/test/java/Framework/TestController.java
+++ src/test/java/Framework/TestController.java
@@ -6,6 +6,9 @@ import Framework.Exception.UnableToGenerateURLException;
 import Framework.Router.Response;
 import Framework.Router.RouteParameters;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Route(path = "/prefixed", name = "test_")
 public class TestController extends BaseController
 {
@@ -30,6 +33,12 @@ public class TestController extends BaseController
     public Response hello5(RouteParameters parameters)
     {
         return new Response("hello, " + parameters.get("name") + " " + parameters.get("___", "fallback"));
+    }
+
+    @Route(path = "/invalidresponse")
+    public List invalidResponse()
+    {
+        return new ArrayList();
     }
 
     @Route(path = "/transform/{name}")
