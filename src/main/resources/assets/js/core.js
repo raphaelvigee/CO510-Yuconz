75efdab0ddaf5f91ec79bed9521e036e3a89cc9c
Brychan Odlum <brychan@me.com>
2018-03-21 20:19:06 +0000

    Resizing text areas + add a bit of style

--- src/main/resources/assets/js/core.js
+++ src/main/resources/assets/js/core.js
@@ -5,4 +5,16 @@ $(function () {
         }
         $(e.currentTarget).find(".summary").toggle();
     });
+
+    var tx = document.getElementsByTagName('textarea');
+    for (var i = 0; i < tx.length; i++) {
+        tx[i].setAttribute('style', 'height:' + (tx[i].scrollHeight) + 'px;overflow-y:hidden;');
+        tx[i].addEventListener("input", OnInput, false);
+    }
+
+    function OnInput() {
+        this.style.height = 'auto';
+        this.style.height = (this.scrollHeight) + 'px';
+    }
 });
+
